5d0300d30cc455bb97b11e6392a4a5e8ec08b601
vidosits <vidosits>
2025-05-26 13:33:45 +0200

    Add Version to Packages for correct serialization

--- types.go
+++ types.go
@@ -1,7 +1,21 @@
 package recommendation
 
+type VersionSpecifier string
+
+const (
+	UnconstrainedVersion VersionSpecifier = "unconstrained"
+)
+
+type Version struct {
+	Specifier  VersionSpecifier `json:"specifier,omitempty"`
+	Constraint string           `json:"constraint"`
+}
+
 type Package struct {
-	Name string `json:"name"`
+	Name        string  `json:"name"`
+	Version     Version `json:"version,omitempty"`
+	Description string  `json:"description,omitempty"`
+	Channel     string  `json:"channel"`
 }
 
 type Environment struct {
@@ -29,5 +43,5 @@ type Startup struct {
 type ImageConfiguration struct {
 	Environments map[string]Environment `json:"environments"`
 	Startup      Startup                `json:"startup"`
-	User         *UserConfiguration     `json:"user"`
+	User         *UserConfiguration     `json:"user,omitempty,omitzero"`
 }
